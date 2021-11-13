41d3c18740f346dd17c28a76adc097d440baa6a4
Jose Fernandez <josesk20@gmail.com>
2021-11-13 18:06:48 -0500

    0.1b

---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Divbkg = styled.div`
+    background-color: #F2F2F2;
+    `
+export const Sh3 = styled.h3`
+    color: gray;
+    text-align: center;
+    grid-column:1;
+    `;
+
+export const Categorydiv = styled.div`
+    justify-content:center;
+    grid-row: 1
+`
