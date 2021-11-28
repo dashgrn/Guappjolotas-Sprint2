90caab71f14eeb6c6af9c0776e22853a6e760dc2
Jose Fernandez <josesk20@gmail.com>
2021-11-28 01:48:06 -0500

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { DivCart } from '../styles/ProductStyles'
-import {Input, Section} from '../styles/HomeStyles'
+import { Input, Section } from '../styles/HomeStyles'
+import axios from 'axios'
+import { URL_SEARCH } from '../helpers/URL'
 
 const Search = () => {
+
+    const [allProds, setAllProds] = useState([])
+
+    const [inputValue, setInputValue] = useState({})
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault()
+        console.log(inputValue.query)
+        await axios.get(`${URL_SEARCH}${inputValue.query}`)
+            .then(res => {
+                let resp = res.data
+                setAllProds(resp)
+            })
+            .catch(err => console.error(err))
+        console.log(allProds)
+    }
+
+    const handleSearchChange = ({ target }) => {
+        setAllProds([])
+        console.log(target.value)
+        console.log(allProds)
+        setInputValue({
+            ...inputValue,
+            [target.name]: target.value
+        })
+    }
+
     return (
         <div>
             <div className="header">
@@ -20,9 +49,15 @@ const Search = () => {
             </div>
 
             <div>
-                <Section>
-                    <Input placeholder="Sabor de guajolota, bebidas..."></Input>
-                </Section>
+                <form onSubmit={(evt) => handleSubmit(evt)}>
+                    <Section>
+                        <Input
+                            onChange={(evt) => handleSearchChange(evt)}
+                            name="query"
+                            placeholder="Sabor de guajolota, bebidas..."></Input>
+                    </Section>
+                </form>
+
             </div>
         </div>
     )
